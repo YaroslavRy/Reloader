6d7f85bdd798b97d385bfa9ad11d723eb0b975e4
Nemo <yar.ry@yandex.ua>
2017-12-31 11:42:53 +0200

    init commit

---
+++ popup.js
@@ -0,0 +1,22 @@
+function settingChanged() {
+	var type = this.id;
+	var setting = this.value;
+	var strTime = setting.toString()
+	var s = strTime.split('.')
+	var minutes = s[0];
+	var seconds = s[1];
+	var time = minutes * 60 * 1000 + seconds * 1000
+
+	setTimeout(function run() {
+		chrome.tabs.reload();
+		setTimeout(run, time);
+	}, time);
+
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	var inputs = document.querySelectorAll('input');
+	for (var i = 0; i < inputs.length; i++) {
+		inputs[i].addEventListener('change', settingChanged);
+	}
+});
